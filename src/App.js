b681291e0c6a50c9fe84340e1f345bc8a1935056
nandkishorr <rachulr7@gmail.com>
2023-04-11 18:25:01 +0530

    project created

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactjgsdiauhda
         </a>
       </header>
     </div>
