d95038d8d94fa7e00514f6ab7043f03ff03673d7
nandkishorr <rachulr7@gmail.com>
2023-04-15 22:23:37 +0530

    eight

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import styles from "./Styles/About.module.css"
 function About() {
   return (
-    <div>
-      hello about
+    <div className={styles.body}>
+      <div className={styles.top}>
+        <h1 className={styles.heading}>ABOUT</h1>
+      </div>
+      <div className={styles.content}></div>
+      <div className={styles.bottom}></div>
     </div>
   )
 }
