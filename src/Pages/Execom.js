780381b1a187dfac81ca97e7ff53a0b9b286fc2c
NivedGanga <nivedganga@gmail.com>
2023-04-16 00:06:41 +0530

    execom page modifiied

--- src/Pages/Execom.js
+++ src/Pages/Execom.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ExecomPic from '../Components/Execom/ExecomPic'
+import Heading from '../Components/Heading'
 function Execom() {
   return (
     <div className='execom container-fluid'>
       <div className='row m-5'>
+        <Heading title='Execom' />
           <ExecomPic link="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-614810.jpg&fm=jpg"/>
-          <ExecomPic />
-          <ExecomPic />
-          <ExecomPic />
+          <ExecomPic  link="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-614810.jpg&fm=jpg"/>
+          <ExecomPic  link="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-614810.jpg&fm=jpg"/>
+          <ExecomPic  link="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-614810.jpg&fm=jpg"/>
       </div>
     </div>
   )
